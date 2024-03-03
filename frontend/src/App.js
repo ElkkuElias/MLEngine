9696b4f10595425bd7819c8537ada22bf64568bc
ElkkuElias <elias.vuorinenkso@gmail.com>
2024-03-03 23:03:05 +0200

    Removal of dangling submodule references

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import SurveyComponent from './survey.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    
+        <SurveyComponent />
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
