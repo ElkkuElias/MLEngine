667b5c0fa84539e88e911e9ab9b1f1aca1141813
ElkkuElias <111981846+ElkkuElias@users.noreply.github.com>
2024-03-10 23:54:00 +0200

    Update testRunner.py

--- backend/testRunner.py
+++ backend/testRunner.py
@@ -1,21 +1,17 @@
 import unittest
 import xmlrunner
 
-# Import  test modules
-
 def run_tests():
-    """Runs the unittest test suite and outputs a JUnit XML report."""
-    # Defines the directory where you want to save the JUnit XML reports
+    """Runs the unittest test suite and outputs JUnit XML reports."""
+    # Define the directory where Junit XML reports go
     output_dir = 'test-reports'
 
-    # Discovers and load all unittest test cases
+    # Discover and load all unittest test cases
     loader = unittest.TestLoader()
     suite = loader.discover('tests', pattern='Test*.py')
 
-    # Rusn the tests and output to JUnit XML
-    with open(f"{output_dir}/unittest_results.xml", "w") as output:
-        xmlrunner.XMLTestRunner(output=output).run(suite)
-
+    # Run the test and output to JUnit XML in the specified directory
+    xmlrunner.XMLTestRunner(output=output_dir).run(suite)
 
 if __name__ == '__main__':
     run_tests()
