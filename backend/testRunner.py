5c689b4a54496f94e03441a43b31313769425033
ElkkuElias <elias.vuorinenkso@gmail.com>
2024-03-12 11:25:02 +0200

    some commenting

--- backend/testRunner.py
+++ backend/testRunner.py
@@ -1,8 +1,13 @@
 import unittest
 import xmlrunner
+import coverage
 
 def run_tests():
-    """Runs the unittest test suite and outputs JUnit XML reports."""
+    """Runs the unittest test suite with coverage monitoring and outputs JUnit XML reports."""
+    # Start coverage collection
+    cov = coverage.coverage()
+    cov.start()
+
     # Define the directory where Junit XML reports go
     output_dir = 'test-reports'
 
@@ -10,8 +15,16 @@ def run_tests():
     loader = unittest.TestLoader()
     suite = loader.discover('tests', pattern='Test*.py')
 
-    # Run the test and output to JUnit XML in the specified directory
+    # Run the test suite
     xmlrunner.XMLTestRunner(output=output_dir).run(suite)
+
+    # Stop coverage collection
+    cov.stop()
+    cov.save()
+
+    # Generate coverage reports
+    cov.html_report()  # HTML report
+    cov.report()  # Standard output report
 
 if __name__ == '__main__':
     run_tests()
