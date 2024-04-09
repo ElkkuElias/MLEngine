6f30b4c9e64741e891ab3132560e54d3d0577419
ElkkuElias <111981846+ElkkuElias@users.noreply.github.com>
2024-04-09 11:57:52 +0300

    Update FlaskServer.py

--- backend/FlaskServer.py
+++ backend/FlaskServer.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import torch
 from DataProcessing import QuestionnaireNN,scaler,le
-import mysql
+
 
 app = Flask(__name__)
 CORS(app)
