634058846bcd1cf37f5953448b9fba497aa38be3
ElkkuElias <111981846+ElkkuElias@users.noreply.github.com>
2024-05-06 18:20:52 +0300

    -

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://avnadmin:AVNS_19nEvhDyeK4ZRpyv_VE@mysql-2e3947b-elias-8bb3.f.aivencloud.com:15255/projectdatabase'
+    SQLALCHEMY_DATABASE_URI = ''
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_ORIGINS = "http://localhost:3000"
 
--- backend/testingconfig.py
+++ backend/testingconfig.py
@@ -1,4 +1,4 @@
 class TestingConfig:
-    SQLALCHEMY_DATABASE_URI = 'mysql://avnadmin:AVNS_19nEvhDyeK4ZRpyv_VE@mysql-2e3947b-elias-8bb3.f.aivencloud.com:15255/projectdatabasetest'
+    SQLALCHEMY_DATABASE_URI = ''
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_ORIGINS = "http://localhost:3000"
